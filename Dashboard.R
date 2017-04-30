8faa92653cd24d3356d0e1e9138db199a6aa4075
xeaphii <muhammadzeeshan020@gmail.com>
2017-04-30 07:03:55 +0200

    Ntework done

--- Dashboard.R
+++ Dashboard.R
@@ -9,6 +9,14 @@ library(rpart.plot)
 library(mlbench)
 library(caret)
 set.seed(0)
+library(GGally)
+
+devtools::install_github("briatte/ggnet")
+library(ggnet)
+
+library(network)
+library(sna)
+library(ggplot2)
 
 
 # create a connection
@@ -95,7 +103,13 @@ ui <- dashboardPage(
     
     # Second tab content
     tabItem(tabName = "tb_analysis",
-            h2("Widgets tab content")),
+            sidebarLayout(
+              sidebarPanel(
+                h2("Second analysis tab")
+              ),
+              
+              mainPanel(plotOutput("network_plot"))
+            )),
     
     # Second tab content
     tabItem(
@@ -169,6 +183,46 @@ server <- function(input, output) {
     data <- histdata[seq_len(input$slider)]
     hist(data)
   })
+  
+  
+  
+  
+  
+  sql_command <-
+    "SELECT   type,next_type,   avg(next_row - action_date) no_days FROM (        SELECT          bill_id,          action_date,          type,          lead(action_date, 1)          OVER (            PARTITION BY bill_id            ORDER BY action_date ) next_row,          lead(type, 1)          OVER (            PARTITION BY bill_id            ORDER BY action_date ) next_type        FROM actions        ORDER BY bill_id, action_date      ) tb1   INNER JOIN bills     ON bills.id = tb1.bill_id   where next_type is not NULL group by type,next_type ;"
+  #sql_command <- "SELECT         type,         avg(             CASE WHEN type = 'BecameLaw'               THEN                 next_row - action_date             ELSE 0 END) became_law,         avg(             CASE WHEN type = 'Calendars'               THEN                 next_row - action_date             ELSE 0 END) Calendars,         avg(             CASE WHEN type = 'Committee'               THEN                 next_row - action_date             ELSE 0 END) Committee,         avg(             CASE WHEN type = 'Discharge'               THEN                 next_row - action_date             ELSE 0 END) Discharge,         avg(             CASE WHEN type = 'Floor'               THEN                 next_row - action_date             ELSE 0 END) Floor,         avg(             CASE WHEN type = 'IntroReferral'               THEN                 next_row - action_date             ELSE 0 END) IntroReferral,         avg(             CASE WHEN type = 'NotUsed'               THEN                 next_row - action_date             ELSE 0 END) NotUsed,         avg(             CASE WHEN type = 'President'               THEN                 next_row - action_date             ELSE 0 END) President,         avg(             CASE WHEN type = 'ResolvingDifferences'               THEN                 next_row - action_date             ELSE 0 END) ResolvingDifferences,         avg(             CASE WHEN type = 'Veto'               THEN                 next_row - action_date             ELSE 0 END) Veto       FROM (SELECT               bill_id,               action_date,               type,               lead(action_date, 1)               OVER (                 PARTITION BY bill_id                 ORDER BY action_date ) next_row,               lead(type, 1)               OVER (                 PARTITION BY bill_id                 ORDER BY action_date ) next_type             FROM actions             ORDER BY bill_id, action_date) tb1 INNER JOIN bills ON bills.id = tb1.bill_id       WHERE next_type IS NOT NULL       GROUP BY type"
+  bills_inp <- dbGetQuery(con, sql_command)
+  #bills_inp <- bills_inp[,-1]
+  #nodes<-rownames(bills_inp)
+  
+  v <- as.data.frame(nodes)
+  names(v)
+  e <- bills_inp[, -3]
+  names(e)
+  
+  net = network(e,
+                 directed = TRUE)
+  
+  
+  
+  
+  
+  
+  
+  output$network_plot <-renderPlot( ggnet2(
+    net,
+    directed = TRUE,
+    arrow.size = 12,
+    arrow.gap = 0.025,
+    color = "grey",
+    label = TRUE,
+    alpha = 0.75,
+    size = 10,
+    edge.alpha = 0.5
+  )
+  )
+  
+  
   observeEvent(c(
     input$no_of_days,
     input$party_type,
