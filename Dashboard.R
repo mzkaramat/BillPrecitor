64707c42914a6c3a7ff0aae5d460d2f63f1b3583
xeaphii <muhammadzeeshan020@gmail.com>
2017-04-30 07:45:23 +0200

    Before testing

--- Dashboard.R
+++ Dashboard.R
@@ -111,7 +111,14 @@ ui <- dashboardPage(
                   "Party type",
                   choices = c("Republican", "Democrats"),
                   selected = "Republican"
-                )
+                ), br(),
+                radioButtons(
+                  "bill_type_net",
+                  "Origin Chamber",
+                  choices = c("Senate", "House"),
+                  selected = "Senate"
+                ),br(),selectInput("state_net", "State",
+                                   choices = sort(unique(bills$state)))
               ),
               
               mainPanel(plotOutput("network_plot", height = 600))
@@ -193,50 +200,55 @@ server <- function(input, output) {
   
   
   observeEvent(
-    input$party_type_net,
-  {
-    filter_pred <- if (input$party_type_net == "Republican")
-      'R'
-    else
-      'D'
-    
-    print(paste("Slider moved ",filter_pred))
-    sql_command = paste("SELECT   type,   next_type,   avg(next_row - action_date) no_days FROM (SELECT         bill_id,         action_date,         type,         lead(action_date, 1)         OVER (           PARTITION BY bill_id           ORDER BY action_date ) next_row,         lead(type, 1)         OVER (           PARTITION BY bill_id           ORDER BY action_date ) next_type       FROM actions       ORDER BY bill_id, action_date) tb1 INNER JOIN bills ON bills.id = tb1.bill_id   inner join sponsors on sponsors.bill_id = bills.id   inner join members on members.bioguideid = sponsors.sponsor_id WHERE next_type IS NOT NULL and members.party = '",filter_pred,"' GROUP BY type, next_type;")
-    #sql_command <-
-     # "SELECT   type,next_type,   avg(next_row - action_date) no_days FROM (        SELECT          bill_id,          action_date,          type,          lead(action_date, 1)          OVER (            PARTITION BY bill_id            ORDER BY action_date ) next_row,          lead(type, 1)          OVER (            PARTITION BY bill_id            ORDER BY action_date ) next_type        FROM actions        ORDER BY bill_id, action_date      ) tb1   INNER JOIN bills     ON bills.id = tb1.bill_id   where next_type is not NULL group by type,next_type ;"
-    #sql_command <- "SELECT         type,         avg(             CASE WHEN type = 'BecameLaw'               THEN                 next_row - action_date             ELSE 0 END) became_law,         avg(             CASE WHEN type = 'Calendars'               THEN                 next_row - action_date             ELSE 0 END) Calendars,         avg(             CASE WHEN type = 'Committee'               THEN                 next_row - action_date             ELSE 0 END) Committee,         avg(             CASE WHEN type = 'Discharge'               THEN                 next_row - action_date             ELSE 0 END) Discharge,         avg(             CASE WHEN type = 'Floor'               THEN                 next_row - action_date             ELSE 0 END) Floor,         avg(             CASE WHEN type = 'IntroReferral'               THEN                 next_row - action_date             ELSE 0 END) IntroReferral,         avg(             CASE WHEN type = 'NotUsed'               THEN                 next_row - action_date             ELSE 0 END) NotUsed,         avg(             CASE WHEN type = 'President'               THEN                 next_row - action_date             ELSE 0 END) President,         avg(             CASE WHEN type = 'ResolvingDifferences'               THEN                 next_row - action_date             ELSE 0 END) ResolvingDifferences,         avg(             CASE WHEN type = 'Veto'               THEN                 next_row - action_date             ELSE 0 END) Veto       FROM (SELECT               bill_id,               action_date,               type,               lead(action_date, 1)               OVER (                 PARTITION BY bill_id                 ORDER BY action_date ) next_row,               lead(type, 1)               OVER (                 PARTITION BY bill_id                 ORDER BY action_date ) next_type             FROM actions             ORDER BY bill_id, action_date) tb1 INNER JOIN bills ON bills.id = tb1.bill_id       WHERE next_type IS NOT NULL       GROUP BY type"
-    bills_inp <- dbGetQuery(con, sql_command)
-    #bills_inp <- bills_inp[,-1]
-    #nodes<-rownames(bills_inp)
-    
-    v <- as.data.frame(nodes)
-    names(v)
-    e <- bills_inp[, -3]
-    names(e)
-    
-    net = network(e,
-                  directed = TRUE)
-    
-    
-    
-    
-    
-    
-    
-    output$network_plot <-renderPlot( ggnet2(
-      net,
-      directed = TRUE,
-      arrow.size = 12,
-      arrow.gap = 0.025,
-      color = "grey",
-      label = TRUE,
-      alpha = 0.75,
-      size = 10,
-      edge.alpha = 0.5
-    )
-    )
-    
-  })
+    c(input$party_type_net,input$state_net,input$bill_type_net),
+    {
+      if (input$party_type_net == "Republican")
+        filter_pred <-  'R'
+      else
+        filter_pred <-'D'
+      
+      print(paste("Slider moved ",filter_pred))
+      sql_command = paste("SELECT   type,   next_type,   avg(next_row - action_date) no_days FROM (SELECT         bill_id,         action_date,         type,         lead(action_date, 1)         OVER (           PARTITION BY bill_id           ORDER BY action_date ) next_row,         lead(type, 1)         OVER (           PARTITION BY bill_id           ORDER BY action_date ) next_type       FROM actions       ORDER BY bill_id, action_date) tb1 INNER JOIN bills ON bills.id = tb1.bill_id   inner join sponsors on sponsors.bill_id = bills.id   inner join members on members.bioguideid = sponsors.sponsor_id WHERE next_type IS NOT NULL and members.party = '",filter_pred,"' and members.state = '", input$state_net,"' and bills.originchamber = '",input$bill_type_net,"' GROUP BY type, next_type;",sep = '')
+      #sql_command <-
+      # "SELECT   type,next_type,   avg(next_row - action_date) no_days FROM (        SELECT          bill_id,          action_date,          type,          lead(action_date, 1)          OVER (            PARTITION BY bill_id            ORDER BY action_date ) next_row,          lead(type, 1)          OVER (            PARTITION BY bill_id            ORDER BY action_date ) next_type        FROM actions        ORDER BY bill_id, action_date      ) tb1   INNER JOIN bills     ON bills.id = tb1.bill_id   where next_type is not NULL group by type,next_type ;"
+      #sql_command <- "SELECT         type,         avg(             CASE WHEN type = 'BecameLaw'               THEN                 next_row - action_date             ELSE 0 END) became_law,         avg(             CASE WHEN type = 'Calendars'               THEN                 next_row - action_date             ELSE 0 END) Calendars,         avg(             CASE WHEN type = 'Committee'               THEN                 next_row - action_date             ELSE 0 END) Committee,         avg(             CASE WHEN type = 'Discharge'               THEN                 next_row - action_date             ELSE 0 END) Discharge,         avg(             CASE WHEN type = 'Floor'               THEN                 next_row - action_date             ELSE 0 END) Floor,         avg(             CASE WHEN type = 'IntroReferral'               THEN                 next_row - action_date             ELSE 0 END) IntroReferral,         avg(             CASE WHEN type = 'NotUsed'               THEN                 next_row - action_date             ELSE 0 END) NotUsed,         avg(             CASE WHEN type = 'President'               THEN                 next_row - action_date             ELSE 0 END) President,         avg(             CASE WHEN type = 'ResolvingDifferences'               THEN                 next_row - action_date             ELSE 0 END) ResolvingDifferences,         avg(             CASE WHEN type = 'Veto'               THEN                 next_row - action_date             ELSE 0 END) Veto       FROM (SELECT               bill_id,               action_date,               type,               lead(action_date, 1)               OVER (                 PARTITION BY bill_id                 ORDER BY action_date ) next_row,               lead(type, 1)               OVER (                 PARTITION BY bill_id                 ORDER BY action_date ) next_type             FROM actions             ORDER BY bill_id, action_date) tb1 INNER JOIN bills ON bills.id = tb1.bill_id       WHERE next_type IS NOT NULL       GROUP BY type"
+      print(sql_command)
+      bills_inp <- dbGetQuery(con, sql_command)
+      print(nrow(bills_inp))
+      if (nrow(bills_inp) != 0){
+      #bills_inp <- bills_inp[,-1]
+      nodes<-rownames(bills_inp)
+     # if (nrow(bills_inp) != 0 )then{
+       
+        v <- as.data.frame(nodes)
+        names(v)
+        e <- bills_inp[, -3]
+        names(e)
+        
+        net = network(e,
+                      directed = TRUE)
+        
+        
+        
+        
+        
+        
+        
+        output$network_plot <-renderPlot( ggnet2(
+          net,
+          directed = TRUE,
+          arrow.size = 12,
+          arrow.gap = 0.025,
+          color = "grey",
+          label = TRUE,
+          alpha = 0.75,
+          size = 10,
+          edge.alpha = 0.5
+        )
+        )
+        }
+      
+    })
   
   
   
