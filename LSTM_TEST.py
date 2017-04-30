ab50b3e69de50aa264b0f5f871b8af4856c36ea0
xeaphii <muhammadzeeshan020@gmail.com>
2017-04-30 03:42:41 +0200

    changes

--- LSTM_TEST.py
+++ LSTM_TEST.py
@@ -6,8 +6,6 @@ from keras.layers import Dropout
 from keras.layers import LSTM
 from keras.callbacks import ModelCheckpoint
 from keras.utils import np_utils
-
-
 # load ascii text and covert to lowercase
 filename_text = "/home/hackathon/sandbox/titles.txt"
 raw_text = open(filename_text).read()
@@ -31,8 +29,18 @@ for i in range(0, n_chars - seq_length, 1):
     dataY.append(char_to_int[seq_out])
 n_patterns = len(dataX)
 print ("Total Patterns: ", n_patterns)
-
+# reshape X to be [samples, time steps, features]
+X = numpy.reshape(dataX, (n_patterns, seq_length, 1))
+# normalize
+X = X / float(n_vocab)
+# one hot encode the output variable
+y = np_utils.to_categorical(dataY)
 model = Sequential()
+model.add(LSTM(256, input_shape=(X.shape[1], X.shape[2])))
+model.add(Dropout(0.2))
+model.add(Dense(y.shape[1], activation='softmax'))
+model.compile(loss='categorical_crossentropy', optimizer='adam')
+
 filename = "weights-improvement-07-1.6203.hdf5"
 model.load_weights(filename)
 model.compile(loss='categorical_crossentropy', optimizer='adam')
